864816dd0c9126032e30d70a5a1a7b65d404a33a
wangxiang6 <81965848@qq.com>
2021-04-03 12:06:56 +0800

    跳跃问题

--- src/main/java/leetcode/mid/SearchRange.java
+++ src/main/java/leetcode/mid/SearchRange.java
@@ -1,23 +1,24 @@
 package leetcode.mid;
 
-import leetcode.util.ArrayUtils;
+
+import util.ArrayUtil;
 
 /**
  * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
- *
+ * <p>
  * 你的算法时间复杂度必须是 O(log n) 级别。
- *
+ * <p>
  * 如果数组中不存在目标值，返回 [-1, -1]。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: nums = [5,7,7,8,8,10], target = 8
  * 输出: [3,4]
  * 示例 2:
- *
+ * <p>
  * 输入: nums = [5,7,7,8,8,10], target = 6
  * 输出: [-1,-1]
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -26,12 +27,12 @@ public class SearchRange {
     public static int[] searchRange(int[] nums, int target) {
         int begin = -1, end = -1;
         for (int i = 0; i < nums.length; i++) {
-             if(target == nums[i] ){
-                 end = i;
-                 if (begin == -1){
-                     begin = i;
-                 }
-             }
+            if (target == nums[i]) {
+                end = i;
+                if (begin == -1) {
+                    begin = i;
+                }
+            }
 
         }
         return new int[]{begin, end};
@@ -39,22 +40,22 @@ public class SearchRange {
 
     public static int[] searchRange2(int[] nums, int target) {
         int begin = -1, end = -1;
-        int i = 0, j = nums.length -1,mid ;
-        while(i <= j){
-            mid = (i + j) /2;
+        int i = 0, j = nums.length - 1, mid;
+        while (i <= j) {
+            mid = (i + j) / 2;
             System.out.println(mid);
-            if(nums[mid] > target) {
-                j= --mid;
-            }else if(nums[mid] < target){
+            if (nums[mid] > target) {
+                j = --mid;
+            } else if (nums[mid] < target) {
                 i = ++mid;
-            }else {
+            } else {
                 begin = mid;
                 end = mid;
-                while(begin >= 0 && nums[begin] == target){
+                while (begin >= 0 && nums[begin] == target) {
                     begin--;
                 }
                 begin += 1;
-                while(end < nums.length && nums[end] == target){
+                while (end < nums.length && nums[end] == target) {
                     end++;
                 }
                 end -= 1;
@@ -68,7 +69,7 @@ public class SearchRange {
 
     public static void main(String[] args) {
         int[] result = searchRange2(new int[]{1}, 1);
-        ArrayUtils.printArray(result);
+        ArrayUtil.print(result);
     }
 
 
